6ce881a1730c20273a2c9ab3e7e412cf40ed4135
Thomas Meißner <thomas.meissner@sumup.com>
2022-12-07 13:23:39 +0100

    tuples

---
+++ src/tuples.rs
@@ -0,0 +1,18 @@
+fn tuples() {
+    // tuples are often used for functions with several outputs
+    let my_tuple: (&str, i32) = ("My name", 5000_000);
+
+    println!("The value of idx 0 is {} and the other one is {}.", my_tuple.0, my_tuple.1);
+    println!("This can also be printed as {:?}", my_tuple);
+
+    // deconstruct into multiple scalars individually
+    let (name, salary) = my_tuple;
+
+    // we can also nest tuples
+    let nested_tuple = (4.0, 3, ("My", "Pet"));
+    let element: &str = nested_tuple.2.0; // accessing the element of a nested tuple
+}
+
+fn main() {
+    tuples()
+}
