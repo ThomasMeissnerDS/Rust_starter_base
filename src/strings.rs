127480109ddb803cd35d6d7c0736a55377e764b0
Thomas Meißner <thomas.meissner@sumup.com>
2022-12-07 08:45:34 +0100

    strings

--- src/strings.rs
+++ src/strings.rs
@@ -23,6 +23,24 @@ fn strings() {
     println!("Is string has {} bytes allocated in memory.", other_string.capacity()); // memory consumption
     println!("Does our string contain 'in use'? -  {}", other_string.contains("In use")); // search for substring
 
+    other_string.push_str("     "); // adding white spaces here to have something for trimming
+    other_string.trim(); // We can trim the string
+    other_string.trim().len() // we can also chain methods
+
+    // we can also convert numbers to strings
+    let number: i32 = 45;
+    let num_string: Str = number.to_string(); // conversion to string
+    println!("Is this really a string? - {}", num_string == "45");
+
+    // we can also convert other data types to string
+    a_char: char = "a";
+    let a_string: Str = a_char.to_string();
+    println!("We have a string");
+
+    // We can also just convert a string on the fly
+    fly_string: Str = "On the fly".to_string();
+
+
 }
 
 fn main() {
