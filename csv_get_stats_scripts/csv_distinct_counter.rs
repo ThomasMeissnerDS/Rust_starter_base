89f6d08d58ba5c53bf1f669442e76ef524ac6a77
ThomasMeissnerDS <meissnercorporation@gmx.de>
2023-02-27 07:01:15 +0100

    Update groupby csv file

--- csv_get_stats_scripts/csv_distinct_counter.rs
+++ csv_get_stats_scripts/csv_distinct_counter.rs
@@ -3,20 +3,18 @@ Script that reads a csv in a loop. Takes a column and counts the number of disti
 that column. Accepts two arguments:
 - the column name that shall be counted
 - path to the csv file (has to include .csv)
- */
-use::std::collections::HashMap;
+*/
+use std::collections::HashMap;
 use std::env;
-use csv::ReaderBuilder;
-use csv::Error;
+use csv::{Error, ReaderBuilder};
 
 fn main() -> Result<(), Error> {
     // get command line arguments
-    let args: Vec<String> = env::args().collect();
-    let count_col = String::from(&args[1]);
-    let csv = String::from(&args[2]);
+    let count_col = &env::args().nth(1).expect("count_col not provided");
+    let csv = &env::args().nth(2).expect("file_name not provided");
 
     let mut col_indices: HashMap<String, usize> = HashMap::new();
-    let mut col_counts: HashMap<String, u32> = HashMap::new();
+    let mut col_counts: HashMap<String, Vec<(u32, f64, f64)>> = HashMap::new();
 
     let mut reader = ReaderBuilder::new()
         .flexible(true)
@@ -38,13 +36,26 @@ fn main() -> Result<(), Error> {
         }
         // get all rows except header
         else {
-            let temp_str = String::from(&record[*col_indices.get(&count_col).unwrap()]); // convert to String to be able to deep copy
-            *col_counts.entry(temp_str.to_owned()).or_default() += 1;
+            let count_col_index = *col_indices.get(count_col).unwrap();
+            let group_val = record[count_col_index].to_string();
 
+            // update counts, sum, and mean for the current group value
+            let count = col_counts.entry(group_val.clone())
+                .or_insert_with(|| vec![(0, 0.0, 0.0)])
+                .first_mut().unwrap();
+            count.0 += 1;
+            count.1 += 1.0;
+            count.2 = count.1 / (count.0 as f64);
+
+            // print some stats for each group value
+            if row_idx % 100000 == 0 {
+                println!("Group value: {}, count: {}, sum: {}, mean: {}", group_val, count.0, count.1, count.2);
+            }
         }
 
         row_idx += 1;
     }
+
     println!("The distinct number of {} categories is {}", count_col, col_counts.keys().len());
     Ok(())
 }
--- csv_get_stats_scripts/csv_groupby_simple_stats.rs
+++ csv_get_stats_scripts/csv_groupby_simple_stats.rs
@@ -1,8 +1,8 @@
-use rust_decimal::prelude::*;
-use std::collections::{HashMap, HashSet};
+use std::collections::HashMap;
 use std::env;
 use std::fs::File;
 use std::io::{BufRead, BufReader};
+use std::borrow::Borrow;
 
 fn main() {
     // Parse command line arguments
@@ -22,23 +22,23 @@ fn main() {
     col_indices.insert(groupby_col, headers.iter().position(|&x| x == groupby_col).unwrap());
     col_indices.insert(count_col, headers.iter().position(|&x| x == count_col).unwrap());
 
-    // Loop through remaining rows and accumulate counts, sum, and distinct values for each group
+    // Loop through remaining rows and accumulate counts, sum, and mean for each group
     let mut counts = HashMap::new();
     for line in lines {
         let record = line.unwrap();
         let record: Vec<&str> = record.split(',').collect();
         let group_val = record[*col_indices.get(groupby_col).unwrap()].to_string();
-        let col_val = Decimal::from_str(record[*col_indices.get(count_col).unwrap()]).unwrap_or_else(|_| Decimal::new(0, 0));
-        let (count, sum, values) = counts.entry(group_val.clone()).or_insert((0, Decimal::new(0, 0), HashSet::new()));
-        *count += 1;
-        *sum += col_val;
-        values.insert(col_val);
+        let col_val = record[*col_indices.get(count_col).unwrap()].parse::<f64>().unwrap_or(0.0);
+
+        let group_data = counts.entry(group_val.clone()).or_insert(vec![0.0, 0.0, 0.0]);
+        group_data[0] += 1.0; // increment count
+        group_data[1] += col_val; // add value to sum
+        group_data[2] = group_data[1] / group_data[0]; // calculate mean
     }
 
-    // Print group, count, sum, mean, and distinct count pairs
-    for (group_val, (count, sum, values)) in counts {
-        let mean = if count == 0 { Decimal::new(0, 0) } else { sum / Decimal::new(count, 0) };
-        let distinct_count = values.len();
-        println!("{},{},{},{},{}", group_val, count, sum, mean, distinct_count);
+    // Print group, count, sum, and mean
+    println!("group, count, sum, mean");
+    for (group_val, group_data) in counts {
+        println!("{},{},{},{}", group_val, group_data[0], group_data[1], group_data[2]);
     }
 }
